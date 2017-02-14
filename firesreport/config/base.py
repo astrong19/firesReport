1c69f897b70f05b13a03c12ae362e27df79e0c67
astrong19 <astrong@wri.org>
2017-02-14 05:11:12 -0500

    moving to new repo

---
+++ firesreport/config/base.py
@@ -0,0 +1,12 @@
+from firesreport.utils.files import BASE_DIR
+
+settings={
+    'logging': {
+        'level': 'DEBUG'
+    },
+    'service': {
+        'name': 'firesreport-PYTHON',
+        'uri': 'http://mymachine:5700',
+        'port': 5700
+    }
+}
